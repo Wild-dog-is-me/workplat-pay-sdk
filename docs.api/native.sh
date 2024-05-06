7b1e6d63cb6d834c93686a96aff476e8d531fa6e
zhanghaojian <1427774041@qq.com>
2024-05-06 21:46:32 +0800

    curl测试

--- docs.api/native.sh
+++
@@ -1,4 +0,0 @@
-curl -X POST \
-  https://api.ltzf.cn/api/wxpay/native \
-  -H 'content-type: application/x-www-form-urlencoded' \
-  -d 'mch_id=1676056677&out_trade_no=captain20240506&total_fee=0.01&body=QQ公仔&timestamp=1714995623&notify_url=http://dev.code-farmer.online&sign=C7F0FE0A744F7C074222745682258002'
